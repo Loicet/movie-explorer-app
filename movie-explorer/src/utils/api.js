89f175ba2a101eca90ce7dedfc39f5c8a3884d33
Loicet <l.teta2@alustudent.com>
2025-10-21 18:28:29 +0200

    Added the api fetching file

---
+++ movie-explorer/src/utils/api.js
@@ -0,0 +1,9 @@
+export async function getAllMovies() {
+  const res = await fetch("https://api.tvmaze.com/shows");
+  return await res.json();
+}
+
+export async function getMovieById(id) {
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  return await res.json();
+}
